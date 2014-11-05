bc997bd28c8104bb7849cd8a99d84f3cc5ddf5e5
DobHoy <dob.hoy@gmail.com>
2014-11-05 12:26:31 +0000

    first commit

---
+++ public/app/app.js
@@ -0,0 +1,16 @@
+angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
+  // $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+    });
+  $routeProvider
+    .when('/', {templateUrl: '/partials/main', controller: 'mainCtrl'});
+}]);
+
+angular.module('app').controller('mainCtrl', function($scope){
+  $scope.myVar = "Hello angular";
+
+});
